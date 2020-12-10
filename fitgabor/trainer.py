83b9387c6917b0d797d102c904a4bcec4698f73c
Paweł <ppierzc@gmail.com>
2020-12-10 11:37:25 +0100

    [FIX] remove lr decay parameter

--- fitgabor/trainer.py
+++ fitgabor/trainer.py
@@ -5,7 +5,7 @@ from tqdm import trange
 
 
 def trainer_fn(gabor_generator, model_neuron,
-               epochs=20000, lr=1e-3, lr_decay=1,
+               epochs=20000, lr=1e-3,
                fixed_std=.01,
                save_rf_every_n_epoch=None,
                optimizer=optim.Adam):
@@ -16,8 +16,6 @@ def trainer_fn(gabor_generator, model_neuron,
     pbar = trange(epochs, desc="Loss: {}".format(np.nan), leave=True)
     saved_rfs = []
     for epoch in pbar:
-        lr *= lr_decay
-
         optimizer.zero_grad()
 
         # generate gabor
