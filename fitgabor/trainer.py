5f16fb3253d4bea6f0985faeb76543e46d8286af
Paweł <ppierzc@gmail.com>
2020-12-10 11:25:02 +0100

    [ADD] Changable optimizer and learning rate decay

--- fitgabor/trainer.py
+++ fitgabor/trainer.py
@@ -3,32 +3,40 @@ import torch
 from torch import optim
 from tqdm import trange
 
-def trainer_fn(gabor_generator, model_neuron, 
-               epochs=20000, lr=1e-3, 
-               fixed_std=.01, 
-               save_rf_every_n_epoch=None):
-    
+
+def trainer_fn(gabor_generator, model_neuron,
+               epochs=20000, lr=1e-3, lr_decay=1,
+               fixed_std=.01,
+               save_rf_every_n_epoch=None,
+               optimizer=optim.Adam):
     gabor_generator.apply_changes()
-    
-    optimizer = optim.Adam(gabor_generator.parameters(), lr=lr)
+
+    optimizer = optimizer(gabor_generator.parameters(), lr=lr)
+
     pbar = trange(epochs, desc="Loss: {}".format(np.nan), leave=True)
     saved_rfs = []
     for epoch in pbar:
+        lr *= lr_decay
         optimizer.zero_grad()
-        
+
         # generate gabor
         gabor = gabor_generator()
-        
+
         if fixed_std is not None:
             gabor_std = gabor.std()
             gabor_std_constrained = fixed_std * gabor / gabor_std
-        
+
         loss = -model_neuron(gabor_std_constrained)
+
         loss.backward()
-        optimizer.step()
+
+        def closure():
+            return loss
+
+        optimizer.step(closure)
 
         pbar.set_description("Loss: {:.2f}".format(loss.item()))
-        
+
         if save_rf_every_n_epoch is not None:
             if (epoch % save_rf_every_n_epoch) == 0:
                 saved_rfs.append(gabor.squeeze().cpu().data.numpy())
