59f249d0bb6eb91d0c56a828133526d493fbff26
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2020-12-11 15:10:53 +0100

    Add learning rate scheduler to trainer

--- fitgabor/trainer.py
+++ fitgabor/trainer.py
@@ -5,7 +5,7 @@ from tqdm import trange
 
 
 def trainer_fn(gabor_generator, model_neuron,
-               epochs=20000, lr=1e-3,
+               epochs=20000, lr=5e-3,
                fixed_std=.01,
                save_rf_every_n_epoch=None,
                optimizer=optim.Adam):
@@ -13,9 +13,22 @@ def trainer_fn(gabor_generator, model_neuron,
 
     optimizer = optimizer(gabor_generator.parameters(), lr=lr)
 
+    lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=10)
+    old_lr = lr
+    lr_change_counter = 0
+
     pbar = trange(epochs, desc="Loss: {}".format(np.nan), leave=True)
     saved_rfs = []
     for epoch in pbar:
+        
+        current_lr = optimizer.state_dict()['param_groups'][0]['lr']
+        if old_lr != current_lr:
+            old_lr = current_lr
+            lr_change_counter += 1
+
+        if lr_change_counter > 3:
+            break
+
         def closure():
             optimizer.zero_grad()
 
@@ -33,13 +46,15 @@ def trainer_fn(gabor_generator, model_neuron,
             return loss
 
         loss = optimizer.step(closure)
-        loss = loss.detach().numpy()
 
         pbar.set_description("Loss: {:.2f}".format(loss.item()))
 
         if save_rf_every_n_epoch is not None:
+            gabor = gabor_generator()
             if (epoch % save_rf_every_n_epoch) == 0:
                 saved_rfs.append(gabor.squeeze().cpu().data.numpy())
+
+        lr_scheduler.step(-loss)
 
     gabor_generator.eval();
     return gabor_generator, saved_rfs
