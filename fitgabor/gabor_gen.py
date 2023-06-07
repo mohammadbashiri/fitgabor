3c6d01960520da0c8ae63f3cac355e2bbef65363
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2023-06-07 14:30:57 +0200

    Use normalized pixel positions

--- fitgabor/gabor_gen.py
+++ fitgabor/gabor_gen.py
@@ -3,8 +3,24 @@ from torch import nn
 
 torch.pi = torch.acos(torch.zeros(1)).item() * 2 # which is 3.1415927410125732
 
+def gen_gabor(theta, sigma, Lambda, psi, gamma, center, image_size):
+    
+    sigma_x = sigma
+    sigma_y = sigma / gamma
+
+    ny, nx = image_size
+    (y, x) = torch.meshgrid(torch.linspace(-1, 1, ny), torch.linspace(-1, 1, nx))
+
+    # Rotation
+    x_theta = (x - center[0]) * torch.cos(theta) + (y - center[1]) * torch.sin(theta)
+    y_theta = -(x - center[0]) * torch.sin(theta) + (y - center[1]) * torch.cos(theta)
+
+    gb = torch.exp(-.5 * (x_theta ** 2 / sigma_x ** 2 + y_theta ** 2 / sigma_y ** 2)) * torch.cos(2 * torch.pi / Lambda * x_theta + psi)
+    
+    return gb
+
 class GaborGenerator(nn.Module):
-    def __init__(self, image_size, target_std=1.):
+    def __init__(self, image_size):
         """
         Gabor generator class.
         Params:
@@ -16,45 +32,26 @@ class GaborGenerator(nn.Module):
                 like an aspect ratio of the Gaussian).
             center (tuple of integers): The position of the filter.
             image_size (tuple of integers): Image height and width.
-            target_std:
         Returns:
             2D torch.tensor: A gabor filter.
         """
         
         super().__init__()
-        self.theta = nn.Parameter(torch.rand(1))
-        self.sigma = nn.Parameter(torch.rand(1)+3., requires_grad=False)
-        self.Lambda = nn.Parameter(torch.rand(1)+9.5)
-        self.psi = nn.Parameter(torch.rand(1)*torch.pi/2)
+        self.theta = nn.Parameter(torch.rand(1) * 4*torch.pi - 2*torch.pi)
+        self.sigma = nn.Parameter(torch.rand(1) * .05 + .15, requires_grad=False)
+        self.Lambda = nn.Parameter(torch.rand(1) * .2 + .5)
+        self.psi = nn.Parameter(torch.rand(1)*torch.pi/2) 
         self.gamma = nn.Parameter(torch.zeros(1)+1., requires_grad=False)
         self.center = nn.Parameter(torch.tensor([0., 0.]))
         self.image_size = image_size
-        self.target_std = target_std
     
     def forward(self):
-        return self.gen_gabor()
-    
-    def gen_gabor(self):
-        
         # clip values in reasonable range
-        self.theta.data.clamp_(-torch.pi, torch.pi)
-        self.sigma.data.clamp_(3., min(self.image_size)/2) #min(self.image_size)/7, min(self.image_size)/5) #2)
-        
-        sigma_x = self.sigma
-        sigma_y = self.sigma / self.gamma
-
-        ymax, xmax = self.image_size
-        xmax, ymax = (xmax - 1)/2, (ymax - 1)/2
-        xmin = -xmax
-        ymin = -ymax
-        (y, x) = torch.meshgrid(torch.arange(ymin, ymax+1), torch.arange(xmin, xmax+1))
-
-        # Rotation
-        x_theta = (x - self.center[0]) * torch.cos(self.theta) + (y - self.center[1]) * torch.sin(self.theta)
-        y_theta = -(x - self.center[0]) * torch.sin(self.theta) + (y - self.center[1]) * torch.cos(self.theta)
-
-        gb = torch.exp(-.5 * (x_theta ** 2 / sigma_x ** 2 + y_theta ** 2 / sigma_y ** 2)) * torch.cos(2 * torch.pi / self.Lambda * x_theta + self.psi)
-
+        self.theta.data.clamp_(-2*torch.pi, 2*torch.pi)
+        self.sigma.data.clamp_(.13, 1)
+        self.Lambda.data.clamp_(.2, 2.)
+        self.center.data.clamp_(-.8, .8)
+        gb = gen_gabor(self.theta, self.sigma, self.Lambda, self.psi, self.gamma, self.center, self.image_size)
         return gb.view(1, 1, *self.image_size)
     
     def apply_changes(self):
